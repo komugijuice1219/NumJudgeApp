901ec67bbe635a0210a2ae7bd63c1f7ab0953cae
tao <taozhedan1219@gmail.com>
2024-11-26 10:23:12 +0900

    Initial Commit

---
+++ CalculateApp/CalculateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateAppApp.swift
+//  CalculateApp
+//
+//  Created by たお1219 on 2024/11/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
